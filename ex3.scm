3417f06807e0dc835738272c78c6250a2da7cbd1
YaGiNA <yahyu10101@gmail.com>
2017-07-11 14:28:43 +0900

    WIP ex3-4

--- ex3.scm
+++ ex3.scm
@@ -16,6 +16,8 @@
           (else (map diff ls))
 )))
 
+;(diff fx)
+
 (define tangent-a
   (lambda (a fa)
     ((eval `(lambda (x) ,(diff fa)) (interaction-environment)) a)
@@ -33,7 +35,7 @@
         )
 )))
 
-(tangent 2 fx)
+;(tangent 2 fx)
 
 (define diff2
   (lambda (ls d)
@@ -51,4 +53,44 @@
           (else (map diff2 ls d))
 )))
 
-(diff2 '(* y x) 'x)
+;(diff2 '(* y x) 'x)
+
+(define delete-zero
+  (lambda (lst)
+    (cond
+     ((equal? 0 (car lst)) (cdr lst))
+     (else (cons (car lst) (delete-zero  (cdr lst)))))))
+
+(define simple+
+  (lambda (non-zero-list)
+    (cond ((null? non-zero-list) 0)
+          ((null? (cdr non-zero-list)) (car non-zero-list))
+          (else (eval (cons '+ non-zero-list) (interaction-environment)))
+      
+)))
+
+(define simple-
+  (lambda (lst)
+    (cond ((null? (delete-zero lst)) '-)
+          (else (eval (cons '- (delete-zero lst)) (interaction-environment)))
+)))
+
+(define simple*
+  (lambda (lst)
+    (cond ((or (equal? (car lst) 0) (equal? (cadr lst) 0)) 0)
+          ((equal? (car lst) 1) (cadr lst))
+          ((equal? (car lst) 1) (cadr lst))
+          (else (eval (cons '* lst) (interaction-environment)))
+)))
+    
+(define simple
+  (lambda (lst)
+    (cond
+      ((not(list? lst)) lst)
+      ((equal? '+ (car lst)) (simple+ (delete-zero (cdr lst))))
+      ((equal? '- (car lst)) (simple- (cdr lst)))
+      ((equal? '* (car lst)) (simple* (cdr lst)))
+      (else (map simple lst))
+)))
+
+(simple (diff '(* x 3)))
