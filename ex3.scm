ec26b3295072c8b916d027fac374cf0714ac3663
YaGiNA <yahyu10101@gmail.com>
2017-07-11 12:09:35 +0900

    WIP: ex3-2

--- ex3.scm
+++ ex3.scm
@@ -1,3 +1,5 @@
+(define ** expt)
+(define fx '(+ (** x 3) (* -2 (** x 2)) 9) )
 (define diff
   (lambda (ls)
     (cond
@@ -6,9 +8,27 @@
           ((equal? '+ (car ls)) `(+ ,@(map diff (cdr ls))))
           ((equal? '- (car ls)) `(- ,@(map diff (cdr ls))))
           ((equal? '* (car ls)) 
-            `(+ (* ,@(car (cdr ls)) ,@(diff (cdr (cdr ls)))) (* ,@(diff (car (cdr ls))) ,@(cdr (cdr ls))))
+            `(+ (* ,(car (cdr ls)) ,(diff (car (cddr ls)))) (* ,(diff (car (cdr ls))) ,(car (cddr ls))))
+          )
+          ((equal? '** (car ls)) 
+            `(* ,(car (cddr ls)) (* ,(diff (car (cdr ls))) (** ,(car (cdr ls)) ,(- (car (cddr ls)) 1))))
           )
           (else (map diff ls))
 )))
 
-(diff '(* 10 x))
+(define tangent-a
+  (lambda (a fa)
+    ((eval `(lambda (x) ,(diff fa)) (interaction-environment)) a)
+))
+(define tangent-fa
+  (lambda (a fa)
+    ((eval `(lambda (x) ,fa) (interaction-environment)) a)
+))
+
+(define tangent
+  (lambda (a fa)
+    (let ((fda (tangent-a a fa)) (fa_val (tangent-fa a fa)))
+      (+ fa_val (- (* x fda) (* fda a)))
+)))
+
+(tangent 2 fx)
