ba927f72f88aecee44a727431eb470025f9f3caa
YaGiNA <yutachihaya@gmail.com>
2017-08-09 21:54:44 +0900

    :bug:Debug of ex3

--- ex3.scm
+++ ex3.scm
@@ -1,6 +1,6 @@
 ;; ex3-1
 (define ** expt)
-(define fx '(+ (** x 4) (* -2 (** x 2)) 9) )
+(define fx-orig '(+ (** x 6) (* -2 (** x 2)) 3) )
 (define diff
   (lambda (ls)
     (cond
@@ -12,32 +12,36 @@
             `(+ (* ,(car (cdr ls)) ,(diff (car (cddr ls)))) (* ,(diff (car (cdr ls))) ,(car (cddr ls))))
           )
           ((equal? '** (car ls)) 
-            `(* ,(car (cddr ls)) (* ,(diff (car (cdr ls))) (** ,(car (cdr ls)) (- ,(car (cddr ls)) 1))))
+            `(* ,(car (cddr ls)) (* ,(diff (car (cdr ls))) (** ,(car (cdr ls)) ,(- (car (cddr ls)) 1))))
           )
           (else (map diff ls))
 )))
- 
-(diff fx)
+
+(diff 'x)
+(diff '(+ x 5))
+(diff '(+ (** x 2) (- (* 4 x) 3))) 
+(diff fx-orig)
 
 ;; ex3-2
-(define tangent-a
+(define tangent-fa
   (lambda (a fa)
     ((eval `(lambda (x) ,(diff fa)) (interaction-environment)) a)
 ))
-(define tangent-fa
+(define tangent-a
   (lambda (a fa)
     ((eval `(lambda (x) ,fa) (interaction-environment)) a)
 ))
 
 (define tangent
   (lambda (a fa)
-    `(+ (* ,(tangent-a a fa) x)
-        ,(let ((fda (tangent-a a fa)) (fa_val (tangent-fa a fa)))
+    `(+ (* ,(tangent-fa a fa) x)
+        ,(let ((fda (tangent-fa a fa)) (fa_val (tangent-a a fa)))
           (- fa_val (* fda a))
         )
 )))
 
-(tangent 2 fx)
+(tangent 2 '(+ (** x 3) (* -2 (** x 2)) 9))
+(tangent 2 fx-orig)
 
 ;; ex3-3
 (define diff2
@@ -51,11 +55,14 @@
             `(+ (* ,(car (cdr ls)) ,(diff2 (car (cddr ls)) d)) (* ,(diff2 (car (cdr ls)) d) ,(car (cddr ls))))
           )
           ((equal? '** (car ls)) 
-            `(* ,(car (cddr ls)) (* ,(diff2 (car (cdr ls)) d) (** ,(car (cdr ls)) (- ,(car (cddr ls)) 1))))
+            `(* ,(car (cddr ls)) (* ,(diff2 (car (cdr ls)) d) (** ,(car (cdr ls)) ,(- (car (cddr ls)) 1))))
           )
           (else (map diff2 ls d))
 )))
 
-(diff2 '(* y x) 'x)
+(diff2 'x 'x)
+(diff2 'y 'x)
+(diff2 '(* x y) 'x)
+(diff2 '(+ (* y x) (- (** y 2) x)) 'y)
 
 ;; ex3-4: retired
